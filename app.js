1f7a7ec4a08f3653bb1c08253dba99fc39094843
Soumya Agarwal <soumya@Soumyas-MacBook-Pro.local>
2024-06-16 21:20:13 +0530

    added client side form validations and added a basic error handler

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
-const db  = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {
     console.log("Database connection");
@@ -16,6 +17,7 @@ db.once("open", () => {
 
 
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -28,7 +30,7 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
+    res.render('campgrounds/index', { campgrounds });
 })
 
 app.get('/campgrounds/new', (req, res) => {
@@ -40,10 +42,14 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground });
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+        next(e)
+    }
 })
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
@@ -61,6 +67,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+})
+
+app.use((err, req, res, next) => {
+    res.send('Oh boy, something went wrong!');
 })
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers')
 const Campground = require('../models/campground');
@@ -6,7 +7,7 @@ const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
-const db  = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => {
     console.log("Database connection");
@@ -14,13 +15,38 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+const getUnsplashImage = async () => {
+    const url = 'https://api.unsplash.com/collections/483251/photos?id=483251';
+    const headers = {
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID ui08PZRnyJuh02O6hRbXmpIeSe-n1pYCE64HDz6DnWQ"
+    }
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'Accept-Version': 'v1',
+                'Authorization': 'Client-ID ui08PZRnyJuh02O6hRbXmpIeSe-n1pYCE64HDz6DnWQ'
+            }
+        });
+        const randomPhotoIndex = Math.floor(Math.random() * (response.data.length));
+        return response.data[randomPhotoIndex].urls.small;
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+}
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: await getUnsplashImage(),
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius nam facere fugiat accusantium commodi amet, nulla rerum saepe a quisquam provident recusandae itaque vero? Quis veniam rerum corrupti voluptatibus ducimus?',
+            price
         });
         await camp.save();
     }
